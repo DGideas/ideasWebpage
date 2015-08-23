5c51aa46ca1c66ab3b7cef2fa0d8a81bd4ffb742
王万霖 <dgideas@outlook.com>
2015-08-23 08:27:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<?php require_once('header.php'); ?>
+<?php require_once('include.php'); ?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+	<head>
+    <?php require_once('header.php'); ?>
+  </head>
   <body>
     <div class="container">
+      <?php include_once("banner.php"); ?>
+			
+			<?php include_once("footer.php"); ?>
     </div>
   </body>
 </html>
