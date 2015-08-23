b4f29fc2a6e2dfb68d16c46d2eb5aa79f8ccace7
王万霖 <dgideas@outlook.com>
2015-08-23 08:28:20 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-  <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
@@ -10,4 +8,3 @@
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  </head>
