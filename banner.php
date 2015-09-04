89b5b1034bdb3dbf1fc464410a8aed65ed7a7b80
王万霖 <dgideas@outlook.com>
2015-09-04 09:12:40 +0800

    删除注释

--- banner.php
+++ banner.php
@@ -1,6 +1,5 @@
 			<nav class="navbar navbar-inverse navbar-fixed-top">
 			  <div class="container-fluid">
-			    <!-- Brand and toggle get grouped for better mobile display -->
 			    <div class="navbar-header">
 			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 			        <span class="sr-only">Toggle navigation</span>
@@ -11,8 +10,6 @@
 				  <span class="glyphicon glyphicon-superscript navbar-brand" aria-hidden="true"></span> 
 			      <a class="navbar-brand" href="#">Your Website</a>
 			    </div>
-			
-			    <!-- Collect the nav links, forms, and other content for toggling -->
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul class="nav navbar-nav">
 			        <li class="active"><a href="#"><?php print($GLOBALS["title"]); ?><span class="sr-only">(current)</span></a></li>
@@ -46,6 +43,6 @@
 			            <li><a href="#">注销</a></li>
 			        </li>
 			      </ul>
-			    </div><!-- /.navbar-collapse -->
-			  </div><!-- /.container-fluid -->
+			    </div>
+			  </div>
 			</nav>
