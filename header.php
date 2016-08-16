6920fd1f013e61e756dffe3516e5b44ae4bf2643
王万霖 <dgideas@outlook.com>
2016-08-16 09:45:22 +0800

    update jQuery library version

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<title><?php print($GLOBALS["title"]); ?></title>
 	    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	    <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="//cdn.bootcss.com/jquery/3.1.0/jquery.min.js"></script>
 	    <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<style type="text/css">
 		body { 
